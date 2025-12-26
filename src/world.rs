28d52f65c4450cda4f566770beb046666673a2ff
horup <horup@deepstacker.com>
2025-12-26 19:45:31 +0100

    refactor: reorganize imports and enhance World struct methods

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,8 @@
 use std::collections::VecDeque;
 
-use crate::{Event, thing::Thing, tile::Tile};
+use glam::IVec2;
+
+use crate::{Event, thing::Thing, tile::{self, Tile}};
 
 #[derive(Default, Clone)]
 pub struct World {
@@ -16,16 +18,35 @@ pub struct World {
 }
 
 impl World {
+    /// Clear the world of all things and tiles
     pub fn clear(&mut self) {
         self.things.clear();
         self.tiles = Default::default();
     }
 
+    /// Get a mutable reference to the player thing
     pub fn player_mut(&mut self) -> Option<&mut Thing> {
         self.things.get_mut(self.player)
     }
 
+    /// Get a reference to the player thing
     pub fn player(&self) -> Option<&Thing> {
         self.things.get(self.player)
+    }
+
+    /// Get all entities within a certain radius of a tile position
+    pub fn get_entities(&self, tile_pos:IVec2, radius:f32, vec:&mut Vec<slotmap::DefaultKey>) {
+        let mut entities = Vec::new();
+        let s = radius.ceil() as i32;
+        for y in -s..=s {
+            for x in -s..=s {
+                let cell = tile_pos + glam::IVec2::new(x, y);
+                if let Some(tile) = self.tiles.get(cell) {
+                    for (entity_id, _) in tile.entities.iter() {
+                        entities.push(entity_id);
+                    }
+                }
+            }
+        }
     }
 }
