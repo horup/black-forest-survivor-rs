0000fb95af2ab0d79e0fa1912762df60bcde1fa8
horup <horup@deepstacker.com>
2026-01-01 22:20:22 +0100

    refactor despawn_system to ensure entity is removed from its tile before despawning

--- game_core/src/systems/despawn.rs
+++ game_core/src/systems/despawn.rs
@@ -2,6 +2,18 @@ use crate::{DespawnEvent, systems::Ctx};
 
 pub fn despawn_system(event: &DespawnEvent, ctx: &mut dyn Ctx) {
     let world = ctx.world_mut();
+    
+    // Get the entity's position before removing it
+    if let Some(entity) = world.entity(event.entity_id) {
+        let tile_pos = entity.pos.truncate().as_ivec2();
+        
+        // Remove entity from its tile
+        if let Some(tile) = world.tiles.get_mut(tile_pos) {
+            tile.entities.remove(&event.entity_id);
+        }
+    }
+    
+    // Remove entity from the world
     if world.entities.remove(event.entity_id).is_some() {
         dbg!("Despawned entity:", event.entity_id);
     }
